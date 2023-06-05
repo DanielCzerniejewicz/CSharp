233ac67b00998d378af9296a266dae4c896e65f6
Daniel Czerniejewicz <61429464+DanielCzerniejewicz@users.noreply.github.com>
2023-06-05 10:58:33 +0200

    05.06.2023
    Zachłanne Wydawanie Reszty

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿void grzebien(int[] T, int gap)
+{
+    bool swapped = true;
+    int tempik;
+    int n = gap;
+    while (gap > 1 || swapped)
+    {
+        gap = gap * 10 / 13;
+        if (gap == 0)
+        {
+            gap = 1;
+        }
+        swapped = false;
+        for (int i = 0; i < n-gap;i++)
+        {
+            if (T[i] > T[i + gap])
+            {
+                tempik = T[i];
+                T[i] = T[i + gap];
+                T[i + gap] = tempik;
+                swapped = true;
+            }
+        }
+    }
+}
